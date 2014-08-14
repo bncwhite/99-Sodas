dd457445be2ccd3cd23ef75ebde7bc1976d1117e
bncwhite <codebybrad@icloud.com>
2014-08-14 11:39:08 -0500

    3 challenges
    More for loop examples added

--- 99 Sodas/BWViewController.m
+++ 99 Sodas/BWViewController.m
@@ -21,6 +21,41 @@
     for (int bottlesOfSoda = 99; bottlesOfSoda >= 0; bottlesOfSoda--) {
         NSLog(@"There are %i bottles of soda on the wall.", bottlesOfSoda);
     }
+    // 1.
+    [self countDown:3];
+    
+    // 2.
+    [self printTopNumber:6 andLowNumber:2];
+    
+    //
+    [self factoredInt:2];
+}
+// 1.
+- (void) countDown:(int)number {
+    for (int i = number; i > 0; i--) {
+        NSLog(@"%i", i);
+    }
+    NSLog(@" ");
+}
+
+// 2.
+- (void) printTopNumber:(int) number1 andLowNumber:(int) number2 {
+    for (int i = number1; i >= number2; i--) {
+        NSLog(@"%i", i);
+    }
+    NSLog(@" ");
+}
+
+// 3.
+- (int) factoredInt:(int)number
+{
+    int answer = number;
+    for (int i = number; i > 1; i--) {
+        answer = answer * (i - 1);
+        NSLog(@"%i", answer);
+    }
+    NSLog(@" ");
+    return answer;
 }
 
 - (void)didReceiveMemoryWarning
